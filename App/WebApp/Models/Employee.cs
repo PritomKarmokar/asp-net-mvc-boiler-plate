24ad636c1dd04da4becb0081c2dae9954a848798
pritom <pritomkarmokar99@gmail.com>
2024-02-23 01:55:49 +0600

    Adding Data Annotations and Connection Strings

--- App/WebApp/Models/Employee.cs
+++ App/WebApp/Models/Employee.cs
@@ -1,9 +1,13 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
 
     }
